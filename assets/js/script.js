72703962c3bf6b5972be15e5a32ad619b25c12a8
Juan Carlos Gutierrez <jc.gutierrezs@gmail.com>
2022-04-20 10:46:02 -0400

    Smooth scroll aplicado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
 $("#carouselExampleControls").carousel({
     interval: 4000
   })
+
+
+  $("a").click(function(event) {
+    if (this.hash !== "") {
+        event.preventDefault();
+
+        var scrolear = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(scrolear).offset().top
+        }, 1500, function() {
+            window.location.hash = scrolear;
+        });
+    }
+});
